f81414318bf2ff601242db7538fe614d0af27bed
Martin Frisk <yfflan@gmail.com>
2024-10-22 00:44:59 +0200

    simple event loop

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use macroquad::prelude::*;
 use hexaroni::game::Game;
 use hexaroni::renderer::draw;
 
+enum Event {
+    Exit,
+}
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "Hexaroni".to_owned(),
@@ -16,17 +20,35 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     // set_fullscreen(true);
-    let time = Instant::now();
+    let start_time = Instant::now();
     let game = Game::new();
     loop {
-        draw::background();
-        for tile in game.board.tiles.iter() {
-            draw::tile(tile, time.elapsed().as_secs_f32());
+        let curr_time = start_time.elapsed().as_secs_f32();
+
+        match get_event() {
+            Some(Event::Exit) => break,
+            _ => {}
         }
-        for object in game.board.objects.iter() {
-            draw::object(object, time.elapsed().as_secs_f32());
-        }
-        
+
+        render(&game, curr_time);
         next_frame().await
+    }
+}
+
+
+fn get_event() -> Option<Event> {
+    if is_key_pressed(KeyCode::Escape) {
+        return Some(Event::Exit);
+    }
+    None
+}
+
+fn render(game: &Game, time: f32) {
+    draw::background();
+    for tile in game.board.tiles.iter() {
+        draw::tile(tile, time);
+    }
+    for object in game.board.objects.iter() {
+        draw::object(object, time);
     }
 }
